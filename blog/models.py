ebf832f0a82062b5f7e874f62c49dcf57df4b45d
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-08-24 14:56:37 +0200

    Tutorial model

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,10 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ManyToManyField(Category)
