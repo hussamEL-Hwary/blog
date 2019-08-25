3f4b1909d4d5917391b7fdc829d59ba7a54b8020
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-08-25 21:59:04 +0200

    admin models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from django.db import models
+from .models import Category, Tutorial
+from tinymce.widgets import TinyMCE
 # Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()}
+    }
+
+
+admin.site.register(Category)
+admin.site.register(Tutorial, TutorialAdmin)
