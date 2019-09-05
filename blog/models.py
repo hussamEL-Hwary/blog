3227195fd0bca5a1bc72f4faa105d8234f575e34
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-09-05 19:39:18 +0200

    category slug

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    category_slug = models.CharField(max_length=200, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
