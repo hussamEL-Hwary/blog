92207acbab2a0ebcca2c568673193e9143898cec
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-09-06 14:36:36 +0200

    Message form

---
+++ blog/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+class MessageForm(forms.Form):
+    author = forms.CharField(
+        max_length=60,
+        required=False,
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Name not required"
+        })
+    )
+
+    email = forms.CharField(
+        required=False,
+         widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "placeholder": "Mail"
+        })
+    )
+
+    body = forms.CharField(
+        widget=forms.Textarea(attrs={
+            "class": "form-control",
+            "placeholder": "leave a comment !"
+        })
+    )
