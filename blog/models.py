5e6902192bf083ac02a6b1382f08faeccd1887d3
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-09-10 18:06:06 +0200

    add Comment model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -18,6 +20,11 @@ class Tutorial(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ManyToManyField(Category)
+    comments = models.IntegerField(default=0)
+
+    def commented(self):
+        self.comments += 1
+        self.save(update_fields=['comments'])
 
     def __str__(self):
         return self.title
@@ -30,3 +37,8 @@ class Message(models.Model):
     def __str__(self):
         return self.author
 
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
