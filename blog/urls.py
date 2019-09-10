3b5eed9d616941f6e64dab9a78e46a561ab99787
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-09-10 18:17:32 +0200

    login url

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
 
+
 app_name = 'blog'
 urlpatterns=[
     path("", views.homepage, name="homepage"),
     path("post/<int:post_id>", views.blog_post, name="post"),
     path("category/<slug>", views.category_posts, name="category_posts"),
     path("contact-me/", views.visitor_message, name="message"),
+    path("login/", views.login, name="login")
 ]
