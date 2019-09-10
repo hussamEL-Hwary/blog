d894fac8e83244c00b22084f1e34f37cf5ee0d7c
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-09-10 18:16:39 +0200

    social login settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -65,10 +65,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',   
+    'django.contrib.staticfiles',
+    'social_django',   
     'blog.apps.BlogConfig',
     'tinymce',
-    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -94,13 +94,37 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 'social_core.backends.github.GithubOAuth2',  # for Github authentication
+ 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+ 
+ 'django.contrib.auth.backends.ModelBackend',
+)
+
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+# social login
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
+SOCIAL_AUTH_GITHUB_KEY = '72884cdee33968e9b6c9'
+SOCIAL_AUTH_GITHUB_SECRET = '032607e844387e889727329ef93cea38cb059a2d'
+
+SOCIAL_AUTH_FACEBOOK_KEY = '382168616028870'
+SOCIAL_AUTH_FACEBOOK_SECRET = '263c61883e3e8d92ccb4c48895064cbc'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
