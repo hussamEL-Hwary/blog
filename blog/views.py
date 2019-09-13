bd4639a55fa03b4042194d43d74706f8fd6f2329
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-09-13 16:46:42 +0200

    Http404 response

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Category, Tutorial, Message, Comment
 from .forms import MessageForm, CommentForm
 from ratelimit.decorators import ratelimit
