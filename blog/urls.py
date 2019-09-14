9a02972b76b2c522f337c3fd065af5f2231df2a3
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-09-14 02:48:21 +0200

    aboume url

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns=[
     path("post/<int:post_id>", views.blog_post, name="post"),
     path("category/<slug>", views.category_posts, name="category_posts"),
     path("contact-me/", views.visitor_message, name="message"),
-    path("login/", views.login, name="login")
+    path("login/", views.login, name="login"),
+    path("about-me/", views.about_me, name="aboutme")
 ]
