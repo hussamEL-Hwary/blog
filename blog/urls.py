a42ea2ec9d2b70da8555f1e58cb2a685fdfa0ab4
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-10-04 01:21:34 +0200

    api urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from . import views
 from . import APIviews
+from rest_framework_swagger.views import get_swagger_view
 
-
-
+schema_view = get_swagger_view(title="Swagger Docs")
 app_name = 'blog'
 urlpatterns=[
     path("", views.homepage, name="homepage"),
@@ -14,6 +14,7 @@ urlpatterns=[
     path("about-me/", views.about_me, name="aboutme"),
     path("post/JSON/", APIviews.PostView.as_view(), name="posts_json"),
     path("post/<slug>/JSON/", APIviews.PostDetail.as_view(), name="post_detail"),
-    path("category/JSON/", APIviews.CategoryView.as_view(), name="categories_json")
-
+    path("category/JSON/", APIviews.CategoryView.as_view(), name="categories_json"),
+    path("category/<slug>/JSON/", APIviews.CategoryPostsView.as_view(), name="category_posts_json"),
+    path("docs/", schema_view, name="api_docs"),
 ]
