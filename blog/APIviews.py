c299c5dfca5afc3b503f0e972de31a448f86a320
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-10-03 16:55:51 +0200

    post detail view

--- blog/APIviews.py
+++ blog/APIviews.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from .models import Tutorial
 from .serializers import TutorialSerializer
 from rest_framework.response import Response
+from rest_framework.views import status
 
 class PostView(APIView):
     def get(self, request):
@@ -9,3 +10,15 @@ class PostView(APIView):
         latest_posts = Tutorial.objects.all().order_by('-created_at')[:10]
         data = TutorialSerializer(latest_posts, many=True).data
         return Response(data)
+
+
+class PostDetail(APIView):
+    def get(self, request, slug):
+        try:
+            a_post = Tutorial.objects.get(slug=slug)
+            return Response(TutorialSerializer(a_post).data)
+        except Tutorial.DoesNotExist:
+            return Response(
+                data={"message": "post not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
